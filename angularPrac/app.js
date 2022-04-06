1a473a18d4d477ffb35b1e378e8d7d6c33aa475b
MT <vominhthang061003@gmail.com>
2022-04-06 08:36:44 +0700

    fdx

--- angularPrac/app.js
+++ angularPrac/app.js
@@ -1,10 +1,67 @@
 (function () {
-'use trict';
+'use strict';
     
 angular.module('myFirstApp', [])
+.controller('myFirstController', DIController);
+DIController.$inject= ['$scope', '$filter'];
 
-.controller('myFirstController', function () {
-    
-})
+    function DIController($scope, $filter) {
+        $scope.name= "type a name"
+        $scope.nameNumber= 0
+
+        $scope.number= "type random numbers"
+        $scope.numberName= "..."
+
+        $scope.displayNumber= function () {
+            var totalNameValue= calculateNumberFromString($scope.name);
+            $scope.nameNumber= totalNameValue;
+        }
+        $scope.displayName= function () {
+            var nameString= calculateNameFromNumber($scope.number);
+            $scope.numberName= nameString;
+        }
+
+        function calculateNumberFromString(string) {
+            var totalStringValue= 0
+            if ($scope.name=== "thang") return "hi boss"
+            for (var i= 0; i< string.length; i++) {
+                totalStringValue += string.charCodeAt(i);
+                // totalStringValue *= 37
+            }
+            return totalStringValue;
+        }
+
+        function calculateNameFromNumber(numberID) {
+            var nameString= "ok idk how to make it work lmao"
+            return nameString;
+        }
+
+        $scope.upper= function () {
+            var upCase= $filter('uppercase');
+            $scope.name= upCase($scope.name);
+        }
+
+        $scope.state= "notSus";
+        $scope.buttonState= "pig";
+        $scope.changeState= function () {
+            if ($scope.state== "sus") {
+                $scope.state= "notSus"
+                $scope.buttonState= "pig"
+            } else {
+                $scope.state= "sus"
+                $scope.buttonState= "sus"
+            }
+        }
+
+        $scope.status= function (state) {
+            if ($scope.state== "sus") {
+                return "ඞ"
+            }
+        }
+
+        // $scope.log= function () {
+        //     console.log($scope.state);
+        // }
+    }
 
 })();
